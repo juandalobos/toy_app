734ed8e09b73404c1b95b70c427fc6b4160abf23
juandalobos <juan.villalobos@somosinternet.co>
2025-05-04 17:28:07 -0500

    Define ruta raíz con users#index en toy_app

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
+  root "users#index"
   # root "posts#index"
 end
